82aa800e30e8f39cb481b7ce047f22c8446bf90d
Nesar Ahmed <nesar13@hotmail.com>
2021-02-22 13:23:16 -0500

    added worklist route

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const app=express();
 app.use(bodyParser.urlencoded({extended: true})); 
 
 // creating an array to store the list of toDo list
-var items=[]; 
+let items=[]; 
+let workItems=[]; 
 
 // checks for a view folder in the home directory for a .ejs file
 // ejs allows you to manipulate html from the backend 
@@ -41,24 +42,36 @@ app.get("/", function(req, res) {
     // we want to assign 
     // newListItems is the bullet list that we will use in the html
     
-    res.render("list", {kindOfDay: day, newListItems: items}); 
+    res.render("list", {listTitle: day, newListItems: items}); 
 
 
 });
 
 app.post("/", function(req, res) {
 
-    // geting the text from the submitted text
+ // geting the text from the submitted text
     // note that we are using bodyParser to get the body 
     var item=req.body.newItem; 
 
-    // pushing into array
-    items.push(item); 
+    // depending on the route we are using, we will add to appropriate list
+    if (req.body.list === "Work") {
+        workItems.push(item); 
+        //redirecting to work route
+        res.redirect("/work"); 
 
-    // redirecting to home route
-    res.redirect("/"); 
+    } else {
+        items.push(item); 
+        res.redirect("/"); 
+    }
 
 }); 
+
+app.get("/work", function(req, res) { 
+
+    res.render("list", {listTitle: "Work", newListItems: workItems}); 
+}); 
+
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000"); 
