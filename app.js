b9fcd0045bf921ec51c105c4135cdfe81af3c6fd
Nesar Ahmed <nesar13@hotmail.com>
2021-03-01 13:50:30 -0500

    added separate date.js file to export functions

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 
-const express= require("express"); 
-const bodyParser= require("body-parser"); 
+const express = require("express");
+const bodyParser = require("body-parser");
 const { urlencoded } = require("body-parser");
 
-const app=express(); 
+const date = require(__dirname + "/date.js");
 
-app.use(bodyParser.urlencoded({extended: true})); 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // creating an array to store the list of toDo list
-let items=[]; 
-let workItems=[]; 
+let items = [];
+let workItems = [];
 
 // checks for a view folder in the home directory for a .ejs file
 // ejs allows you to manipulate html from the backend 
 // must be set after express
 
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 
 // we have to indicate that nodejs will use static files in the public folder
 // we are currently using css 
 
-app.use(express.static("public")); 
+app.use(express.static("public"));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 
-   
+
 
     // res.render() function is used to render a view and sends rendered
     // html string to client 
@@ -33,45 +35,47 @@ app.get("/", function(req, res) {
     // kindofday is the variable we use in the ejs file and day is the value
     // we want to assign 
     // newListItems is the bullet list that we will use in the html
-    
-    res.render("list", {listTitle: day, newListItems: items}); 
+    let day = date.getDate();
+
+
+    res.render("list", { listTitle: day, newListItems: items });
 
 
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
 
- // geting the text from the submitted text
+    // geting the text from the submitted text
     // note that we are using bodyParser to get the body 
-    var item=req.body.newItem; 
+    var item = req.body.newItem;
 
     // depending on the route we are using, we will add to appropriate list
     if (req.body.list === "Work") {
-        workItems.push(item); 
+        workItems.push(item);
         //redirecting to work route
-        res.redirect("/work"); 
+        res.redirect("/work");
 
     } else {
-        items.push(item); 
-        res.redirect("/"); 
+        items.push(item);
+        res.redirect("/");
     }
 
-}); 
+});
 
-app.get("/work", function(req, res) { 
+app.get("/work", function (req, res) {
 
-    res.render("list", {listTitle: "Work", newListItems: workItems}); 
-}); 
+    res.render("list", { listTitle: "Work", newListItems: workItems });
+});
 
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
 
     res.render("about");
 })
 
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000"); 
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 
 });
 
--- date.js
+++ date.js
@@ -1,13 +1,37 @@
 
-function getDate() {
+module.exports.getDate = function () {
 
 
-let today = new Date(); 
-let currentDay = today.getDay(); 
+    let today = new Date();
+    let currentDay = today.getDay();
 
 
-// will use this to format date 
-let options = { weekday: 'long', month: 'long', day: 'numeric' };
+    // will use this to format date 
+    let options = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric'
+    };
 
-let day=today.toLocaleDateString("en-US", options); 
+    let day = today.toLocaleDateString("en-US", options);
+
+    return day;
 }
+
+module.exports.getDay = function () {
+
+
+    let today = new Date();
+    let currentDay = today.getDay();
+
+
+    // will use this to format date 
+    let options = {
+        weekday: 'long'
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    return day;
+}
+
